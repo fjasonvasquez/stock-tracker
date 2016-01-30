d8fcb0bb3cad23c0a7faf9722e82c4f9e33726b2
fjasonvasquez <fjvasquez@aol.com>
2016-01-30 01:03:25 -0800

    Many to many associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128072606) do
+ActiveRecord::Schema.define(version: 20160130083243) do
 
   create_table "stocks", force: true do |t|
     t.string   "ticker"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160128072606) do
     t.decimal  "last_price"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "user_stocks", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "stock_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: true do |t|
